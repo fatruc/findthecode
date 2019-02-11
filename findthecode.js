8ce57748e98f860f05db38c3ace7e3a0be762149
Fabrice DARBAS <fabrice.darbas@orange.com>
2019-02-11 10:50:22 +0100

    paris#1 dummy page

---
+++ findthecode.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+    $("#test-the-code").click(function(){
+		var mycode = $("#my-code").val();
+		var solution = $("#solution").val();
+		
+		if(solution===mycode){
+			$(".success").show();
+			$(".failure").hide();
+		} else {
+			$(".success").hide();
+			$(".failure").show();
+		}
+		
+		$('#verification-popup').modal();
+	});
+	
+	$("#reveal-the-code").click(function(){
+		var solution = $("#solution").val();
+		$("#put-the-code-here").html(solution);
+		$("#reveal-popup").modal();
+	})
+	
+	$(".indication").click(function(){
+		$(this).parent().parent().find("img").attr("src",$(this).attr("replacement"));
+	})
+});
