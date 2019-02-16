7b754f8ffb1409f8cb7104eacf4aa26d40491a56
fatruc <fabrice.darbas@gmail.com>
2019-02-16 18:35:37 +0100

    .

--- findthecode.js
+++ findthecode.js
@@ -1,9 +1,24 @@
 $( document ).ready(function() {
+	String.prototype.hashCode = function() {
+	  var hash = 0, i, chr;
+	  if (this.length === 0) return hash;
+	  for (i = 0; i < this.length; i++) {
+		chr   = this.charCodeAt(i);
+		hash  = ((hash << 5) - hash) + chr;
+		hash |= 0; // Convert to 32bit integer
+	  }
+	  return hash;
+	};
+	
+	
     $("#test-the-code").click(function(){
 		var mycode = $("#my-code").val();
 		var solution = $("#solution").val();
 		
-		if(solution===mycode){
+			console.log(solution);
+			console.log(mycode.toLowerCase().hashCode());
+			
+		if(solution==mycode.toLowerCase().hashCode()){
 			$(".success").show();
 			$(".failure").hide();
 		} else {
