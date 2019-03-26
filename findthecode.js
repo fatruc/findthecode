94435c7af30b616423f6dec68c3bda2d6d5ff43e
Fabrice DARBAS <fabrice.darbas@orange.com>
2019-03-26 11:08:30 +0100

    analytics

--- findthecode.js
+++ findthecode.js
@@ -28,7 +28,7 @@ $( document ).ready(function() {
 	
 		var audio = new Audio(contextPath+'/secrets/zelda_secret_sound.mp3');
 		audio.play();				
-		gtag('set', { 'secret': secret });
+		ga('send', 'pageview', "secrets/"+secret+".html");
 		$('#secret-popup').modal();
 	}
 	
@@ -126,8 +126,6 @@ $( document ).ready(function() {
 	
 	initLanguage();
 	
-	$('iframe').iFrameResize( [{options}] );
-
 	
 	
 });
