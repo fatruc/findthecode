98d872aab42622e5bec99a2e155d9234528c9c16
fatruc <fabrice.darbas@gmail.com>
2019-04-04 22:53:43 +0200

    keyboard improvment

--- findthecode.js
+++ findthecode.js
@@ -154,10 +154,10 @@ $( document ).ready(function() {
 	  onKeyPress: button => onKeyPress(button),
 	    layout: {
     default: [
-      "1 2 3 4 5 6 7 8 9 0 - = {backspace} {arrowup}",
-      "q w e r t y u i o p [ ] \\ {arrowleft}",
-      "a s d f g h j k l ; ' {arrowdown}",
-      "z x c v b n m , . / {arrowright}" 
+      "1 2 3 4 5 6 7 8 9 0  {backspace} {arrowup}",
+      "q w e r t y u i o p {arrowleft}",
+      "a s d f g h j k l {arrowdown}",
+      "z x c v b n m  {arrowright}" 
       
 		]}
 	};
